d9b7ec760c4cd0171ffff1f4706882d0aab45434
benyaminlarian <bookbl.lxb@gmail.com>
2023-05-13 18:03:14 +0330

    change color on hover

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
 <template>
-  <h1>hello world</h1>
+    <div :class="{active:isActive,disactive:!isActive}" @mouseover="changeColor()"></div>
+    <h1>hello {{ name }} {{ lastName }} {{ age }} randomNum: {{ random }}</h1>
+    <button @click="randomNumber()">click me</button>
 </template>
 
 <script>
+export default{
+  data () {
+    return {
+      name: 'benyamin',
+      lastName: 'larian',
+      age: 20,
+      random: null,
+      isActive: true,
+    };
+  },
+  methods:{
+    randomNumber(){
+      this.random=Math.random()
+    },
+    changeColor(){
+      this.isActive = !(this.isActive)
+    }
+  }
+}
 
 </script>
 
 <style>
+.active {
+  background-color: black;
+  width: 100px;
+  height: 100px;
+}
+
+.disactive {
+  background-color: red;
+  width: 100px;
+  height: 100px;
+}
+
 
 </style>
